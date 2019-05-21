5d6b9dcf13c08344de9c70c586c01790bbc9dd7c
Ali <alialbaker2007@hotmail.co.uk>
2019-05-21 10:00:21 +0100

    Add form validation

--- modules/custom/rsvplist/src/Form/RSVPForm.php
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -27,6 +27,19 @@ class RSVPForm extends FormBase
     /**
      * (@inheritdoc)
      */
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        $value = $form_state->getValue('email');
+        if ($value == ! \Drupal::service('email.validator')->isValid($value)) {
+            $form_state->setErrorByName('email', t('The email address %mail is not valid.', [
+                '%mail' => $value,
+            ]));
+        }
+    }
+
+    /**
+     * (@inheritdoc)
+     */
     public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state)
     {
         drupal_set_message(t('The form is working.'));
