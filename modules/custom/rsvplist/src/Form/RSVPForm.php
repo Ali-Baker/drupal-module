4633d3cae50a2c378aa65c2c33d3a89080f43895
Ali <alialbaker2007@hotmail.co.uk>
2019-05-21 09:36:34 +0100

    Add form

---
+++ modules/custom/rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @file
+ *  Contains \Drupal\rsvplist\Form\RSVPForm
+ */
+
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides an RSVP Email form.
+ */
+class RSVPForm extends FormBase
+{
+
+    /**
+     * (@inheritdoc)
+     */
+    public function getFormId()
+    {
+        return 'rsvplist_email_form';
+    }
+
+    /**
+     * (@inheritdoc)
+     */
+    public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state)
+    {
+        drupal_set_message(t('The form is working.'));
+    }
+
+    /**
+     * (@inheritdoc)
+     */
+    public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state)
+    {
+        $node = \Drupal::routeMatch()->getParameter('node');
+        $nid = $node->nid->value;
+        $form['email'] = [
+            '#title'       => t('Email Address'),
+            '#type'        => 'textfield',
+            '#size'        => 25,
+            '#description' => t("we'll send update to the email address your provide."),
+            '#required'    => true,
+        ];
+
+        $form['submit'] = [
+            '#type'  => 'submit',
+            '#value' => t('RSVP'),
+        ];
+
+        $form['nid'] = [
+            '#type'  => 'hidden',
+            '#value' => $nid,
+        ];
+
+        return $form;
+    }
+}
